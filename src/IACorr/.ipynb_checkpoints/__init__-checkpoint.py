94ac63e7db67d6b776c1638e83e00510345434f4
Elizabeth J. Gonzalez <egonzalez@pic.es>
2025-09-10 00:06:50 +0200

    Initialize repository.

---
+++ src/IACorr/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,9 @@
+from .compute_IA_corr_from_data import (
+    make_randoms_lightcone,
+    compute_2p_corr,
+    make_randoms_box,
+)
+
+__all__ = ["make_randoms_lightcone", "compute_2p_corr", "make_randoms_box"]
+
+__version__ = "0.1.0"
---
+++ tests/test_import.py
@@ -0,0 +1,4 @@
+# tests/test_import.py
+def test_import():
+    import IACorr
+    assert IACorr is not None
